319212c8fa5d1a2bd08a5a7f1f9a8505a15f8c50
NiSHANT867 <136857527+NiSHANT867@users.noreply.github.com>
2023-07-07 09:52:13 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+
+n1 = int(input("Enter n1: "))
+n2 = int(input("Enter n2: "))
+n3 = int(input("Enter n3: "))
+
+
+average = (n1 + n2 + n3) / 3
+
+
+print(f"The average of {n1}, {n2}, and {n3} is {average}")
+
+
+
+
+
+
+
+
+
+
