1106e8e112a23d675bbbe3aef199a3fba9470215
NiSHANT867 <136857527+NiSHANT867@users.noreply.github.com>
2023-07-07 19:24:21 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,159 @@
+#1.Arithmetic operators
+a = 100
+b = 50
+
+# Addition
+c = a + b
+print(c)  
+
+# Subtraction
+d = a - b
+print(d)  
+
+# Multiplication
+e = a * b
+print(e)  
+
+# Division
+f = a / b
+print(f)  
+
+# Modulus
+h = a % b
+print(h)  
+
+# Floor Division
+g = a // b
+print(g) 
+
+# Exponentiation
+i = a ** b
+print(i)  
+
+#2.Asssignment operators
+x = 20
+y = 5
+# Assign
+x = y
+print(x)  
+
+# Add and Assign
+x = 20
+y = 5
+x += y
+print(x)  
+
+# Subtract and Assign
+x = 20
+y = 5
+x -= y
+print(x)  
+
+# Multiply and Assign
+x = 20
+y = 5
+x *= y
+print(x) 
+
+# Divide and Assign
+x = 20
+y = 5
+x /= y
+print(x)  
+
+# Modulus and Assign
+x = 20
+y = 5
+x %= y
+print(x)  
+
+# Exponentiate and Assign
+x = 20
+y = 5
+x **= y
+print(x)  
+
+#3.Comparison operators
+n = 20
+m = 10
+# Equal to
+print(n == m)  
+
+# Not equal to
+print(n != m)  
+
+# Greater than
+print(n > m)  
+
+# Less than
+print(n < m)  
+
+# Greater than or equal to
+print(n >= m)  
+
+# Less than or equal to
+print(n <= m)  
+
+
+#4.Logical operators
+p = True
+q = False
+# Logical AND
+print(p and q)  
+
+# Logical OR
+print(p or q)  
+
+# Logical NOT
+print(not q) 
+
+
+#5.Bitwise operators
+u = 10  
+v = 7  
+
+# Bitwise AND
+print(u & v)  
+
+# Bitwise OR
+print(u | v)  
+
+# Bitwise XOR
+print(u ^ v)  
+
+# Bitwise NOT
+print(~u) 
+
+# Bitwise Left Shift
+print(u << 2)  
+
+# Bitwise Right Shift
+print(u >> 2) 
+
+
+#6.Identical operators
+
+x = 10
+y = 10
+z = 7
+
+# is operator
+print(x is y)  
+
+# is not operator
+print(x is not z)  
+
+
+#7.Membership operators
+o = [1, 2, 3, 4, 5]
+# In operator
+print(10 in o)  
+
+# Not in operator
+print(9 not in o)  
+
+
+
+
+
+
