ab0df9c08e3bffc420b23dfc2de31b5db8220a29
NiSHANT867 <136857527+NiSHANT867@users.noreply.github.com>
2023-07-09 23:58:11 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,48 @@
+# 1.pass
+n="nishant"
+for i in n:
+    if i=="h":
+        pass
+    else:
+        print(i)  
+
+# 2.break
+n="nishant"
+for i in n:
+    if i=="h":
+        break
+    else:
+        print(i)    
+
+# 3.continue
+n="nishant"
+for i in n:
+    if i=="h":
+        continue
+    else:
+        print(i)  
+
+
+# 4.while with else
+a=1
+while a<=5:
+    if a==3:
+        
+        pass
+        
+    else:
+        print(a)
+    a+=1
+else:print("This has else block😀😃😄😁🤪😭😤") 
+
+#5.for with else
+n="nishant"
+for i in n:
+    if i=="h":
+        continue
+    else:
+        print(i)
+        print("🫵")
+        
+else:
+    print("it has else block")  
